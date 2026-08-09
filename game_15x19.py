7483383acb8210a3759681f750b50d0c2ad84ad6
nguyen05566 <javjp606@gmail.com>
2026-08-09 19:35:26 +0700

    Fix game_15x19.py: restore full working version with proper method names

--- game_15x19.py
+++ game_15x19.py
@@ -1,61 +1,117 @@
 # -*- coding: utf-8 -*-
-"""Board game 15x19 (Caro/Rule-8) for AlphaZero training"""
+"""Board game 15x19 (Caro/Rule-8) for AlphaZero training
+Adapted from junxiaosong/AlphaZero_Gomoku
+"""
 from __future__ import print_function
 import numpy as np
-class Board:
-    def __init__(self,**kw):
-        self.width=int(kw.get('width',15))
-        self.height=int(kw.get('height',19))
-        self.states={};self.n=int(kw.get('n_in_row',5));self.pl=[1,2]
-    def iboard(self,sp=0):
-        if self.width<self.n or self.height<self.n:raise Exception('board too small')
-        self.cp=self.pl[sp];self.av=list(range(self.width*self.height));self.states={};self.lm=-1
-    def mtl(self,m):return[m//self.width,m%self.width]
-    def ltm(self,l):
-        if len(l)!=2:return-1
-        m=l[0]*self.width+l[1];return m if 0<=m<self.width*self.height else-1
-    def cs(self):
-        s=np.zeros((4,self.height,self.width))
+
+
+class Board(object):
+    """Board for 15x19 Caro game with rule 8"""
+
+    def __init__(self, **kwargs):
+        self.width = int(kwargs.get('width', 15))
+        self.height = int(kwargs.get('height', 19))
+        self.states = {}
+        self.n_in_row = int(kwargs.get('n_in_row', 5))
+        self.players = [1, 2]
+
+    def init_board(self, start_player=0):
+        if self.width < self.n_in_row or self.height < self.n_in_row:
+            raise Exception('board width and height can not be less than {}'.format(self.n_in_row))
+        self.current_player = self.players[start_player]
+        self.availables = list(range(self.width * self.height))
+        self.states = {}
+        self.last_move = -1
+
+    def move_to_location(self, move):
+        h = move // self.width
+        w = move % self.width
+        return [h, w]
+
+    def location_to_move(self, location):
+        if len(location) != 2:
+            return -1
+        h = location[0]
+        w = location[1]
+        move = h * self.width + w
+        if move not in range(self.width * self.height):
+            return -1
+        return move
+
+    def current_state(self):
+        square_state = np.zeros((4, self.height, self.width))
         if self.states:
-            mk,vk=zip(*self.states.items())
-            mv=np.array(list(mk));pv=np.array(list(vk))
-            mc=mv[pv==self.cp];mo=mv[pv!=self.cp]
-            s[0][mc//self.width,mc%self.width]=1.0
-            s[1][mo//self.width,mo%self.width]=1.0
-            if self.lm>=0:s[2][self.lm//self.width,self.lm%self.width]=1.0
-        if len(self.states)%2==0:s[3][:,:]=1.0
-        return s[:,::-1,:]
-    def dm(self,m):
-        self.states[m]=self.cp;self.av.remove(m)
-        self.cp=self.pl[0]if self.cp==self.pl[1]else self.pl[1];self.lm=m
-    def haw(self):
-        w,h,s,n=self.width,self.height,self.states,self.n
-        mv=list(set(range(w*h))-set(self.av))
-        if len(mv)<n*2-1:return False,-1
-        for m in mv:
-            y,x=m//w,m%w;p=s[m]
-            if x<=w-n and all(s.get(i,-1)==p for i in range(m,m+n)):return True,p
-            if y<=h-n and all(s.get(i,-1)==p for i in range(m,m+n*w,w)):return True,p
-            if x<=w-n and y<=h-n and all(s.get(i,-1)==p for i in range(m,m+n*(w+1),w+1)):return True,p
-            if x>=n-1 and y<=h-n and all(s.get(i,-1)==p for i in range(m,m+n*(w-1),w-1)):return True,p
-        return False,-1
-    def ge(self):
-        win,wr=self.haw()
-        if win:return True,wr
-        elif not self.av:return True,-1
-        return False,-1
-    def gcp(self):return self.cp
-class Game:
-    def __init__(self,b,**kw):self.board=b
-    def ssp(self,pl,sh=0,t=1e-3):
-        self.board.iboard();sts,mp,cp=[],[],[]
+            moves, players = np.array(list(zip(*self.states.items())))
+            move_curr = moves[players == self.current_player]
+            move_oppo = moves[players != self.current_player]
+            square_state[0][move_curr // self.width, move_curr % self.width] = 1.0
+            square_state[1][move_oppo // self.width, move_oppo % self.width] = 1.0
+            if self.last_move >= 0:
+                square_state[2][self.last_move // self.width, self.last_move % self.width] = 1.0
+        if len(self.states) % 2 == 0:
+            square_state[3][:, :] = 1.0
+        return square_state[:, ::-1, :]
+
+    def do_move(self, move):
+        self.states[move] = self.current_player
+        self.availables.remove(move)
+        self.current_player = (self.players[0] if self.current_player == self.players[1] else self.players[1])
+        self.last_move = move
+
+    def has_a_winner(self):
+        width = self.width
+        height = self.height
+        states = self.states
+        n = self.n_in_row
+        moved = list(set(range(width * height)) - set(self.availables))
+        if len(moved) < self.n_in_row * 2 - 1:
+            return False, -1
+        for m in moved:
+            h = m // width
+            w = m % width
+            player = states[m]
+            if (w in range(width - n + 1) and len(set(states.get(i, -1) for i in range(m, m + n))) == 1):
+                return True, player
+            if (h in range(height - n + 1) and len(set(states.get(i, -1) for i in range(m, m + n * width, width))) == 1):
+                return True, player
+            if (w in range(width - n + 1) and h in range(height - n + 1) and len(set(states.get(i, -1) for i in range(m, m + n * (width + 1), width + 1))) == 1):
+                return True, player
+            if (w in range(n - 1, width) and h in range(height - n + 1) and len(set(states.get(i, -1) for i in range(m, m + n * (width - 1), width - 1))) == 1):
+                return True, player
+        return False, -1
+
+    def game_end(self):
+        win, winner = self.has_a_winner()
+        if win:
+            return True, winner
+        elif not len(self.availables):
+            return True, -1
+        return False, -1
+
+    def get_current_player(self):
+        return self.current_player
+
+
+class Game(object):
+    def __init__(self, board, **kwargs):
+        self.board = board
+
+    def start_self_play(self, player, is_shown=0, temp=1e-3):
+        self.board.init_board()
+        p1, p2 = self.board.players
+        states, mcts_probs, current_players = [], [], []
         while True:
-            m,ms=pl.get_action(self.board,temp=t,return_prob=1)
-            sts.append(self.board.cs());mp.append(ms);cp.append(self.board.cp)
-            self.board.dm(m);end,wr=self.board.ge()
+            move, move_probs = player.get_action(self.board, temp=temp, return_prob=1)
+            states.append(self.board.current_state())
+            mcts_probs.append(move_probs)
+            current_players.append(self.board.current_player)
+            self.board.do_move(move)
+            end, winner = self.board.game_end()
             if end:
-                wz=np.zeros(len(cp))
-                if wr!=-1:
-                    wz[np.array(cp)==wr]=1.0
-                    wz[np.array(cp)!=wr]=-1.0
-                pl.reset_player();return wr,zip(sts,mp,wz)
+                winners_z = np.zeros(len(current_players))
+                if winner != -1:
+                    winners_z[np.array(current_players) == winner] = 1.0
+                    winners_z[np.array(current_players) != winner] = -1.0
+                player.reset_player()
+                return winner, zip(states, mcts_probs, winners_z)
